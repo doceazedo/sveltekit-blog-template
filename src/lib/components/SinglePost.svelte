3561bb3184b72844ddc01481fbf8b5c6883a27d6
doceazedo <me@doceazedo.com>
2023-08-10 20:33:17 -0300

    Add mdsvex

---
+++ src/lib/components/SinglePost.svelte
@@ -0,0 +1,64 @@
+<script>
+	export let title = '';
+</script>
+
+<article class="post">
+	<header class="header">
+		<p class="pretitle">PRESS RELEASE</p>
+		<p class="date">10 de Agosto, 2023</p>
+		<hgroup>
+			<h1 class="title">{title}</h1>
+			<p class="subtitle">Services revenue reaches new all-time high</p>
+		</hgroup>
+	</header>
+	<hr />
+	<div class="content">
+		<slot />
+	</div>
+</article>
+
+<style lang="scss">
+	@import '../scss/vars';
+
+	.post {
+		width: 70ch;
+		margin: 0 auto;
+	}
+
+	.header {
+		padding: 4rem 0;
+	}
+
+	hgroup {
+		margin-top: 1.5rem;
+	}
+
+	.pretitle {
+		font-size: 0.75rem;
+		text-transform: uppercase;
+		font-weight: 600;
+		color: $dark-grey;
+	}
+
+	.title {
+		font-size: 3rem;
+		font-weight: 600;
+		color: $black;
+	}
+
+	.subtitle {
+		font-size: 1.5rem;
+		font-weight: 400;
+		color: $black;
+	}
+
+	.date {
+		font-size: 12px;
+		color: $dark-grey;
+	}
+
+	hr {
+		border-top: 1px solid rgba($dark-grey, 0.1);
+		margin-bottom: 4rem;
+	}
+</style>
---
+++ src/routes/blog/hello-world/+page.md
@@ -0,0 +1,5 @@
+---
+title: Usando Memojis para abrilhantar seus designs
+---
+
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi porro, enim, aspernatur quae esse magni alias excepturi maxime vero, cum dolor saepe voluptatem magnam odio doloremque deserunt debitis inventore repudiandae!
