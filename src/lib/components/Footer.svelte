b43ce7fee636e1c2e32aea368f68eb3ff14f982b
doceazedo <me@doceazedo.com>
2023-08-09 23:38:07 -0300

    Add Footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,15 @@
+<footer class="footer">Desenvolvido com 💙 por DoceAzedo</footer>
+
+<style lang="scss">
+	@import '../scss/vars';
+
+	.footer {
+		width: 100%;
+		text-align: center;
+		background-color: $darkest-grey;
+		margin-top: 4rem;
+		color: #fff;
+		font-size: 0.75rem;
+		padding: 1rem;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,10 @@
 	import '$lib/scss/minireset.scss';
 	import '$lib/scss/global.scss';
 	import Container from '$lib/components/Container.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 </script>
 
 <Container>
 	<slot />
 </Container>
+<Footer />
